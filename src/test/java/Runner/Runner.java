1d073c257810ec671784d88873b1d4e791e53e92
GHfa97 <66691278+FaouziaGh@users.noreply.github.com>
2026-05-11 11:46:50 +0100

    Update Runner.java

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -8,10 +8,14 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-		features ="src\\test\\resources\\Features\\Add.feature",
-		glue="StepDef",
-		plugin = {"pretty","html:target/cucumber/report.html"}
-		) 
+        features ="src\\test\\resources\\Features\\Add.feature",
+        glue="StepDef",
+        plugin = {
+                "pretty",
+                "html:target/cucumber/report.html",
+                "json:target/cucumber/cucumber.json"
+        }
+)
 public class Runner {
-	
+
 }
