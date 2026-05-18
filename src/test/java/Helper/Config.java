b717a42523a858491bb0b220a5c0145d433096ff
GHfa97 <66691278+FaouziaGh@users.noreply.github.com>
2026-05-18 12:02:03 +0100

    Update Config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -1,82 +1,70 @@
 package Helper;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Config {
-	public static WebDriver driver;
-	
-	public static void maximazwindows() {
-		driver.manage().window().maximize();
-	}
+    public static WebDriver driver;
 
-	public static void maximazwindow() {
-    ChromeOptions options = new ChromeOptions();
-
-    // Detect if running on Jenkins (no display)
-    if (System.getProperty("jenkins") != null
-            || System.getenv("JENKINS_HOME") != null) {
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--disable-gpu");
-        System.out.println("Running in headless mode (Jenkins)");
-    } else {
-        options.addArguments("--start-maximized");
-        System.out.println("Running in normal mode (local)");
+    public static void maximazwindows() {
+        driver.manage().window().maximize();
     }
 
-    driver = new ChromeDriver(options);
-}
-	
-	public static void attent(int s) {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
-	}
-	
-	public static WebElement waitForVisibility(WebElement element, int seconds) {
+    public static void maximazwindow() {
+        ChromeOptions options = new ChromeOptions();
+        if (System.getProperty("jenkins") != null
+                || System.getenv("JENKINS_HOME") != null) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
+            System.out.println("Running in headless mode (Jenkins)");
+        } else {
+            options.addArguments("--start-maximized");
+            System.out.println("Running in normal mode (local)");
+        }
+        driver = new ChromeDriver(options);
+    }
 
-	    WebDriverWait wait = new WebDriverWait(driver,
-	            Duration.ofSeconds(seconds));
+    public static void attent(int s) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
+    }
 
-	    return wait.until(ExpectedConditions.visibilityOf(element));
-	}
-	
-	public static void waitAndClick(WebElement element, int seconds) {
+    public static WebElement waitForVisibility(WebElement element, int seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
 
-	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+    public static void waitAndClick(WebElement element, int seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
 
-	    wait.until(ExpectedConditions.elementToBeClickable(element));
-	}
-	
-	public static void waitForUrlContains(String url, int seconds) {
+    public static void waitForUrlContains(String url, int seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.urlContains(url));
+    }
 
-	    WebDriverWait wait = new WebDriverWait(driver,
-	            Duration.ofSeconds(seconds));
+    public static void jsClick(WebElement el) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", el);
+    }
 
-	    wait.until(ExpectedConditions.urlContains(url));
-	}
-	
-	 public static void jsClick(WebElement el) {
-	        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", el);
-	    }
-
-	    public static WebElement waitAndFind(String text) {
-	        String xpath;
-	        if (text.contains("'")) {
-	            xpath = "//*[contains(text(),\"" + text + "\")]";
-	        } else {
-	            xpath = "//*[normalize-space(text())='" + text + "']";
-	        }
-	        return new WebDriverWait(driver, Duration.ofSeconds(10))
-	            .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
-	    }
-	
+    public static WebElement waitAndFind(String text) {
+        String xpath;
+        if (text.contains("'")) {
+            xpath = "//*[contains(text(),\"" + text + "\")]";
+        } else {
+            xpath = "//*[normalize-space(text())='" + text + "']";
+        }
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+            .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+    }
 }
