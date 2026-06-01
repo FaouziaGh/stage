87be601544d41326dc05e0f8445dbce9a752730d
GHfa97 <66691278+FaouziaGh@users.noreply.github.com>
2026-06-01 15:25:42 +0100

    Update Utils.java

--- src/test/java/Helper/Utils.java
+++ src/test/java/Helper/Utils.java
@@ -16,16 +16,24 @@
 // 	}
 // }
 package Helper;
-import java.io.FileInputStream;
+
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Utils {
     public static String getProperty(String property) throws Exception {
-        InputStream input = new FileInputStream("src/test/resources/Properties/properties");
-        //                                           ^ forward slashes only
+        // Load from classpath - works in CI/local/IDE
+        InputStream input = Utils.class.getClassLoader()
+            .getResourceAsStream("Properties/properties");
+        
+        if (input == null) {
+            throw new IllegalArgumentException("Property file not found on classpath!");
+        }
+        
         Properties properties = new Properties();
         properties.load(input);
+        input.close();
+        
         return properties.getProperty(property);
     }
 }
