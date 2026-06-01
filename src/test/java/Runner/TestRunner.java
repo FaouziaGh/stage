97a867f44d8ad1fff2648c15e97f17e48a5dbeda
GHfa97 <66691278+FaouziaGh@users.noreply.github.com>
2026-06-01 10:34:36 +0100

    Fix file path format in Cucumber options

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -8,7 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        features ="src\\test\\resources\\Features\\Update.feature",
+        //features ="src\\test\\resources\\Features\\Update.feature",
+        features = "src/test/resources/Features/Update.feature",
         glue="StepDef",
         plugin = {
                 "pretty",
