5d4ca3a4bfa8a27a6f7617fd64a85d1c91f273f9
GHfa97 <66691278+FaouziaGh@users.noreply.github.com>
2026-06-01 15:49:33 +0100

    Update LoginStepDef.java

--- src/test/java/StepDef/LoginStepDef.java
+++ src/test/java/StepDef/LoginStepDef.java
@@ -11,30 +11,67 @@ import io.cucumber.java.en.When;
 
 public class LoginStepDef {
 	@Given("admin is on login page") 
-	public void admin_is_on_login_page() throws Exception{
-		//Config.driver = new ChromeDriver();
-		Config.maximazwindow();  // ← this now creates the driver WITH headless flags
-	    
+	public void admin_is_on_login_page() throws Exception {
+		Config.maximazwindow();
 	    Config.driver.get(Utils.getProperty("Login_link"));
 	}
 	
 	@When("admin enter correct username {string} and correct password {string}")
-	public void admin_enter_correct_username_and_correct_password(String uname, String pwd) {
+	public void admin_enter_correct_username_and_correct_password(String uname, String pwd) throws Exception {
 	    LoginPage login = new LoginPage();
 	    login.connect(uname, pwd);
-	    
 	}
 	
 	@Then("admin click on role {string}")
-	public void admin_click_on_role(String roleName) {
+	public void admin_click_on_role(String roleName) throws Exception {
 		LoginPage login = new LoginPage();
 	    login.selectRole(roleName);
 	}
 	
 	@Then("admin is directed to home page that containes MSG {string}")
-	public void admin_is_directed_to_home_page_that_containes_msg(String msg)throws Exception {
+	public void admin_is_directed_to_home_page_that_containes_msg(String msg) throws Exception {
 		LoginPage login = new LoginPage();
 	    login.verifLogin(msg);
 	    Config.driver.quit();
 	}
 }
+// package StepDef;
+
+// import org.openqa.selenium.chrome.ChromeDriver;
+
+// import Helper.Config;
+// import Helper.Utils;
+// import Pages.LoginPage;
+// import io.cucumber.java.en.Given;
+// import io.cucumber.java.en.Then;
+// import io.cucumber.java.en.When;
+
+// public class LoginStepDef {
+// 	@Given("admin is on login page") 
+// 	public void admin_is_on_login_page() throws Exception{
+// 		//Config.driver = new ChromeDriver();
+// 		Config.maximazwindow();  // ← this now creates the driver WITH headless flags
+	    
+// 	    Config.driver.get(Utils.getProperty("Login_link"));
+// 	}
+	
+// 	@When("admin enter correct username {string} and correct password {string}")
+// 	public void admin_enter_correct_username_and_correct_password(String uname, String pwd) {
+// 	    LoginPage login = new LoginPage();
+// 	    login.connect(uname, pwd);
+	    
+// 	}
+	
+// 	@Then("admin click on role {string}")
+// 	public void admin_click_on_role(String roleName) {
+// 		LoginPage login = new LoginPage();
+// 	    login.selectRole(roleName);
+// 	}
+	
+// 	@Then("admin is directed to home page that containes MSG {string}")
+// 	public void admin_is_directed_to_home_page_that_containes_msg(String msg)throws Exception {
+// 		LoginPage login = new LoginPage();
+// 	    login.verifLogin(msg);
+// 	    Config.driver.quit();
+// 	}
+// }
