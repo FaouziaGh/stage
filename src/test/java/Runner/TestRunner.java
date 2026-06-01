2fc148427d06742a005ee997597e0919a21a0f79
GHfa97 <66691278+FaouziaGh@users.noreply.github.com>
2026-06-01 14:30:00 +0100

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,23 +1,20 @@
 package Runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-        //features ="src\\test\\resources\\Features\\Update.feature",
-        features = "src/test/resources/Features/LoginPage.feature",
-        glue="StepDef",
-        plugin = {
-                "pretty",
-                "html:target/cucumber/report.html",
-                "json:target/cucumber/cucumber.json",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        }
+    features = "src/test/resources/Features",   // ← whole folder, not one file
+    glue = "StepDef",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",  // ← was target/cucumber/
+        "json:target/cucumber-reports/cucumber.json",  // ← was target/cucumber/
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    monochrome = true
 )
 public class TestRunner {
-
 }
