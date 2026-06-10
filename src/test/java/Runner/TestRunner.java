ba18fa33b7a11908f797f5ff308b9a77f3479dd3
faouzia97 <faouzia.gharbi111@gmail.com>
2026-06-10 10:37:18 +0100

    update

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,22 +1,21 @@
 package Runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-        features ="src\\test\\resources\\Features\\Add.feature",
-        glue="StepDef",
-        plugin = {
-                "pretty",
-                "html:target/cucumber/report.html",
-                "json:target/cucumber/cucumber.json",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        }
+    features = "src/test/resources/Features",   // ← whole folder, not one file
+    glue = "StepDef",
+    tags = "@filter",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",  // ← was target/cucumber/
+        "json:target/cucumber-reports/cucumber.json",  // ← was target/cucumber/
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    monochrome = true
 )
 public class TestRunner {
-
 }
